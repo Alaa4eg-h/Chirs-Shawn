0886f10ac95e209d2b87bc9efa70056279310a73
Alaa4eg-h <Alaa4eg@outlook.com>
2017-07-28 14:31:47 +0200

    Update JqPlugins.js

--- js/JqPlugins.js
+++ js/JqPlugins.js
@@ -8,14 +8,6 @@ $(document).ready(function () {
         progressThree = $(".skill-three .percentage").attr("data-value") * $(".skill-three .percentage").width() / 100;
 
         // PRELOADER //
-    /*
-        $(".cssload-container").fadeOut(10000, function () {
-            $(".preload-overlay").fadeOut(6000, function () {
-                $("body").css("overflow","auto");
-            });
-        });
-*/
-    
         $(window).on("load", function() {
         var preloader = jQuery('.cssload-container');
         var preloaderArea = jQuery('.preload-overlay');
